ddd277221df190c59adbaecaf36885608232ea6e
inotakuya <i_no_u_u@yahoo.co.jp>
2020-08-07 12:34:27 +0900

    ホーム画面レイアウト作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container text-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>マイアドレス帳</h1>
+        <p>
+          マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。
+        </p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color="info">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
